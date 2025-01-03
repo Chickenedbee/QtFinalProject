ded0958b518da36d17470636ba7451b79becd52c
Chickenedbee <bobobopo.com@gmail.com>
2025-01-03 18:51:48 +0800

    Update widget.h

--- QtFinalProject-branch45/Desktop/game/untitled1/widget.h
+++ QtFinalProject-branch45/Desktop/game/untitled1/widget.h
@@ -3,8 +3,8 @@
 
 #include <QMainWindow>
 #include <QVector>
-#include <QDialog>
-#include <QFormLayout>
+#include <QLineEdit>
+#include <QObject>
 #include <QPushButton>
 #include <QGridLayout>
 #include <QVBoxLayout>
@@ -14,6 +14,7 @@
 #include <QKeyEvent>
 #include <QQueue>
 #include <QPoint>
+#include <QSize>
 #include <QSet>
 #include <QSoundEffect>
 class Widget : public QMainWindow
@@ -29,17 +30,31 @@ private:
     int cols = 10;          // 列數
     int mineCount = 10;     // 地雷數量
     int flagCount = 0;
-    int correctCount = 0;
+    int cerrectCount = 0;
+
+    QLineEdit *rowsInput;
+    QLineEdit *colsInput;
+    QLineEdit *mineCountInput;
+    QWidget *centralWidget;
+    QVBoxLayout *mainLayout;
 
     QGridLayout *layout;          // 網格佈局
     QVector<QVector<int>> grid;  // 儲存遊戲格子狀態，-1 代表地雷
     QVector<QVector<QPushButton*>> buttons;  // 儲存所有按鈕
     QVector<QVector<bool>> flags; // 儲存格子是否放置旗子
 
-    void resetGrid(int row, int col);
-    void onResetButtonClicked();
-    void setButton();
+
     void initializeGame();  // 初始化遊戲
+
+    void theDifficultyWidget(); // 選擇難度介面
+    void setEasy();
+    void setNormal();
+    void setHard();
+    void setCustomise();
+
+    void resetGrid(); // 重置陣列
+    void setButton(); // 設定操控按鈕
+
     void reveal(int row, int col);  // 顯示格子的內容
     bool isValid(int row, int col);  // 檢查格子是否有效
     int countMinesAround(int row, int col);  // 計算周圍地雷數量
